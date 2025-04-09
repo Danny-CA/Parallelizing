ca19b2ddc026b073cd3a9765fb9fc17d202c4ab5
Daniel Camilo Aguilar <105518012+Danny-CA@users.noreply.github.com>
2025-04-09 13:06:10 -0600

    Update text_reader.h

--- text_reader.h
+++ text_reader.h
@@ -1,7 +1,7 @@
 #ifndef TEXT_READER_H
 #define TEXT_READER_H
 
-#define MAX_WORDS 1000
+#define MAX_WORDS 20000
 #define MAX_WORD_LEN 100
 
 int read_words_from_file(const char *filename, char words[][MAX_WORD_LEN]);
