cd46d67d3d2f957334e9a0c0e17dce2473473bdc
Daniel Camilo Aguilar <105518012+Danny-CA@users.noreply.github.com>
2025-05-10 12:57:18 -0600

    Update text_reader.c

--- text_reader.c
+++ text_reader.c
@@ -3,10 +3,9 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_WORD_LEN 100
+#define MAX_WORD_LEN 50
 #define MAX_WORDS 20000
 
-// Lista de stopwords en inglés
 const char *stopwords[] = {
     "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
     "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
@@ -36,40 +35,68 @@ int is_stopword(const char *word) {
             return 1;
     return 0;
 }
-
-int read_words_from_file(const char *filename, char words[][MAX_WORD_LEN]) {
+char (*read_words_from_file(const char *filename, int *word_count))[MAX_WORD_LEN] {
     FILE *file = fopen(filename, "r");
     if (!file) {
         perror("Error opening file");
-        return -1;
+        return NULL;
     }
-
+    int capacity = 1000;
+    int word_index = 0;
+    char (*words)[MAX_WORD_LEN] = malloc(capacity * MAX_WORD_LEN);
+    if (!words) {
+        perror("No se pudo asignar memoria inicial");
+        fclose(file);
+        return NULL;
+    }
     char c;
     char current_word[MAX_WORD_LEN];
-    int char_index = 0, word_index = 0;
+    int char_index = 0;
 
     while ((c = fgetc(file)) != EOF) {
         if (isalpha(c)) {
-            current_word[char_index++] = tolower(c);
+            if (char_index < MAX_WORD_LEN - 1) {
+                current_word[char_index++] = tolower(c);
+            }
         } else {
             if (char_index > 0) {
                 current_word[char_index] = '\0';
                 if (!is_stopword(current_word)) {
+                    if (word_index >= capacity) {
+                        capacity *= 2;
+                        char (*temp)[MAX_WORD_LEN] = realloc(words, capacity * MAX_WORD_LEN);
+                        if (!temp) {
+                            perror("Error al expandir memoria");
+                            free(words);
+                            fclose(file);
+                            return NULL;
+                        }
+                        words = temp;
+                    }
                     strcpy(words[word_index++], current_word);
                 }
                 char_index = 0;
             }
         }
     }
-
-    // Para guardar la última palabra si el archivo no termina en espacio o signo
     if (char_index > 0) {
         current_word[char_index] = '\0';
         if (!is_stopword(current_word)) {
+            if (word_index >= capacity) {
+                capacity *= 2;
+                char (*temp)[MAX_WORD_LEN] = realloc(words, capacity * MAX_WORD_LEN);
+                if (!temp) {
+                    perror("Error al expandir memoria final");
+                    free(words);
+                    fclose(file);
+                    return NULL;
+                }
+                words = temp;
+            }
             strcpy(words[word_index++], current_word);
         }
     }
-
     fclose(file);
-    return word_index;
+    *word_count = word_index;
+    return words;
 }
